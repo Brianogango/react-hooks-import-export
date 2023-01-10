9b785db644294b9fb02f317af9561deac941da92
brian <brian.ogango@student.moringaschool.com>
2023-01-10 16:12:05 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -3,6 +3,8 @@ import howManyParks from "./parks/howManyParks";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
-
+  
   return <h1>Colorado State Parks!</h1>;
+  
 }
+export default ColoradoStateParks;
